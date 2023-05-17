fb18470b00a65d9ae88503ef32ba26c1163382cc
piotrkotela <60201080+piotrkotela@users.noreply.github.com>
2023-05-17 18:35:56 +0200

    Added favicon (#2)

--- src/components/IconMenu.tsx
+++ src/components/IconMenu.tsx
@@ -3,21 +3,29 @@ import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { Session, loadSessionsFromLocalStorage, saveSessionsToLocalStorage } from "../helpers/SessionHelpers";
+import {
+  Session,
+  loadSessionsFromLocalStorage,
+  saveSessionsToLocalStorage,
+} from "../helpers/SessionHelpers";
 import { useState } from "react";
 
 interface IconMenuProps {
-  timerSession: string
-  setTimes: React.Dispatch<React.SetStateAction<number[]>>
+  timerSession: string;
+  setTimes: React.Dispatch<React.SetStateAction<number[]>>;
 }
 
-const IconMenu = ({timerSession, setTimes} : IconMenuProps) => {
+const IconMenu = ({ timerSession, setTimes }: IconMenuProps) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = () => {
     setAnchorEl(null);
     const sessions = loadSessionsFromLocalStorage();
     if (sessions.length === 0) {
@@ -26,7 +34,7 @@ const IconMenu = ({timerSession, setTimes} : IconMenuProps) => {
     const updatedSession: Session = { times: [] };
     sessions[+timerSession - 1] = updatedSession;
     saveSessionsToLocalStorage(sessions);
-    setTimes([])
+    setTimes([]);
   };
 
   return (
@@ -50,10 +58,10 @@ const IconMenu = ({timerSession, setTimes} : IconMenuProps) => {
         open={open}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>Clear session</MenuItem>
+        <MenuItem onClick={handleMenuItemClick}>Clear session</MenuItem>
       </Menu>
     </div>
   );
-}
+};
 
-export default IconMenu
+export default IconMenu;
