ad884169ab90884c3818cb91aef54e4398183a37
Piotr Kotela <pkotela1@gmail.com>
2023-06-25 23:02:46 +0200

    Added 6x6 and 7x7 to scramble gen

--- src/helpers/ScrambleGenerator.ts
+++ src/helpers/ScrambleGenerator.ts
@@ -1,14 +1,18 @@
 const createRandomArray = (
   min: number,
   max: number,
-  length: number
+  length: number,
+  isAdjacent: boolean = false
 ): number[] => {
-  const startArray = [];
-  startArray.push(getRandomInt(min, max));
+  const startArray: number[] = [getRandomInt(min, max)];
   let i = 0;
   while (i < length) {
     const randomInt = getRandomInt(min, max);
-    if (startArray[i] !== randomInt) {
+    if (isAdjacent) {
+      startArray.push(randomInt);
+      i++
+    }
+    else if ((startArray[i] !== randomInt)) {
       startArray.push(randomInt);
       i++;
     }
@@ -20,16 +24,17 @@ const getRandomInt = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-type MapNums = Record<number, string>;
+type mapNumsToMoves = Record<number, string>;
+type MapNumsToMovesModifiersFunctions = Record<number, (move: string) => string>
 
 interface scrambleGenParams {
-  mapNumsToMoves: MapNums;
-  mapNumsToMovesModifiers: MapNums;
+  mapNumsToMoves: mapNumsToMoves;
+  mapNumsToMovesModifiersFunctions: MapNumsToMovesModifiersFunctions;
   scrambleLength: number;
 }
 const scrambleGen = ({
   mapNumsToMoves,
-  mapNumsToMovesModifiers,
+  mapNumsToMovesModifiersFunctions,
   scrambleLength,
 }: scrambleGenParams): string => {
   const scrambleWithoutModifiers = createRandomArray(
@@ -38,17 +43,18 @@ const scrambleGen = ({
     scrambleLength
   ).map((num) => mapNumsToMoves[num as keyof typeof mapNumsToMoves]);
 
-  const modifiers = createRandomArray(
+  const modifiersFunctions = createRandomArray(
     0,
-    Object.keys(mapNumsToMovesModifiers).length - 1,
-    scrambleLength
+    Object.keys(mapNumsToMovesModifiersFunctions).length - 1,
+    scrambleLength,
+    true
   ).map(
     (num) =>
-      mapNumsToMovesModifiers[num as keyof typeof mapNumsToMovesModifiers]
+      mapNumsToMovesModifiersFunctions[num as keyof typeof mapNumsToMovesModifiersFunctions]
   );
   let scramble = "";
   for (let i = 0; i < scrambleLength; i++) {
-    scramble += scrambleWithoutModifiers[i] + modifiers[i] + " ";
+    scramble += modifiersFunctions[i](scrambleWithoutModifiers[i]) + " "  
   }
   return scramble;
 };
@@ -58,9 +64,11 @@ export enum MODES {
   "3x3x3" = "3x3x3",
   "4x4x4" = "4x4x4",
   "5x5x5" = "5x5x5",
+  "6x6x6" = "6x6x6",
+  "7x7x7" = "7x7x7"
 }
 
-const mapNumsToMoves3x3x3andUp: MapNums = {
+const mapNumsToMoves3x3x3andUp: mapNumsToMoves = {
   0: "R",
   1: "U",
   2: "F",
@@ -76,45 +84,75 @@ const scrambleParams: Record<MODES, scrambleGenParams> = {
       1: "U",
       2: "F",
     },
-    mapNumsToMovesModifiers: {
-      0: "'",
-      1: "2",
-      2: "",
+    mapNumsToMovesModifiersFunctions: {
+      0: (move) => move + "'",
+      1: (move) => move + "2",
+      2: (move) => move,
     },
     scrambleLength: 9,
   },
   "3x3x3": {
     mapNumsToMoves: mapNumsToMoves3x3x3andUp,
-    mapNumsToMovesModifiers: {
-      0: "'",
-      1: "2",
-      2: "",
+    mapNumsToMovesModifiersFunctions: {
+      0: (move) => move + "'",
+      1: (move) => move + "2",
+      2: (move) => move,
     },
     scrambleLength: 23,
   },
   "4x4x4": {
     mapNumsToMoves: mapNumsToMoves3x3x3andUp,
-    mapNumsToMovesModifiers: {
-      0: "'",
-      1: "2",
-      2: "w",
-      3: "w'",
-      4: "",
+    mapNumsToMovesModifiersFunctions: {
+      0: (move) => move + "'",
+      1: (move) => move + "2",
+      2: (move) => move + "w",
+      3: (move) => move + "w'",
+      4: (move) => move + "",
     },
     scrambleLength: 40,
   },
   "5x5x5": {
     mapNumsToMoves: mapNumsToMoves3x3x3andUp,
-    mapNumsToMovesModifiers: {
-      0: "'",
-      1: "2",
-      2: "w",
-      3: "w'",
-      4: "w2",
-      5: "",
+    mapNumsToMovesModifiersFunctions: {
+      0: (move) => move + "'",
+      1: (move) => move + "2",
+      2: (move) => move + "w",
+      3: (move) => move + "w'",
+      4: (move) => move + "w2",
+      5: (move) => move,
     },
     scrambleLength: 60,
   },
+  "6x6x6": {
+    mapNumsToMoves: mapNumsToMoves3x3x3andUp,
+    mapNumsToMovesModifiersFunctions: {
+      0: (move) => move + "'",
+      1: (move) => move + "2",
+      2: (move) => move + "w",
+      3: (move) => move + "w'",
+      4: (move) => move + "w2",
+      5: (move) => "3" + move + "w",
+      6: (move) => "3" + move + "w'",
+      7: (move) => "3" + move + "w2",
+      8: (move) => move,
+    },
+    scrambleLength: 80,
+  },
+  "7x7x7": {
+    mapNumsToMoves: mapNumsToMoves3x3x3andUp,
+    mapNumsToMovesModifiersFunctions: {
+      0: (move) => move + "'",
+      1: (move) => move + "2",
+      2: (move) => move + "w",
+      3: (move) => move + "w'",
+      4: (move) => move + "w2",
+      5: (move) => "3" + move + "w",
+      6: (move) => "3" + move + "w'",
+      7: (move) => "3" + move + "w2",
+      8: (move) => move,
+    },
+    scrambleLength: 100,
+  }
 };
 
 type ScrambleGeneratorType = {
@@ -125,7 +163,7 @@ const getParams = (mode: MODES) => {
   const params = scrambleParams[MODES[mode]];
   return {
     mapNumsToMoves: params.mapNumsToMoves,
-    mapNumsToMovesModifiers: params.mapNumsToMovesModifiers,
+    mapNumsToMovesModifiersFunctions: params.mapNumsToMovesModifiersFunctions,
     scrambleLength: params.scrambleLength,
   };
 };
@@ -135,6 +173,8 @@ const ScrambleGenerator: ScrambleGeneratorType = {
   "3x3x3": () => scrambleGen(getParams(MODES["3x3x3"])),
   "4x4x4": () => scrambleGen(getParams(MODES["4x4x4"])),
   "5x5x5": () => scrambleGen(getParams(MODES["5x5x5"])),
+  "6x6x6": () =>  scrambleGen(getParams(MODES["6x6x6"])),
+  "7x7x7": () => scrambleGen(getParams(MODES["7x7x7"]))
 };
 
 export default ScrambleGenerator;
